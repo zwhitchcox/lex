eb70f04c9a28c5095c0007e6bc7299a896f1ebb1
Zane Hitchcox <zwhitchcox@gmail.com>
2015-04-07 20:57:30 -0400

    import/export scripts

---
+++ exportDb.sh
@@ -0,0 +1,2 @@
+mongoexport --db mean-dev --collection algos --out algos.json
+mongoexport --db mean-dev --collection clis  --out clis.json
---
+++ importDb.sh
@@ -0,0 +1,2 @@
+mongoimport --collection clis --file clis.json
+mongoimport --collection algos --file algos.json
